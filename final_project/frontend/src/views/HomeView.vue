4e656953ef14f3a1fde51cd8ad8442b9ae3c65db
Imad Saddik <simad3647@gmail.com>
2024-11-17 15:25:23 +0100

    Initialized vue project

---
+++ final_project/frontend/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <SearchSection />
+</template>
+
+<script>
+import SearchSection from "@/components/SearchSection.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    SearchSection,
+  },
+};
+</script>
