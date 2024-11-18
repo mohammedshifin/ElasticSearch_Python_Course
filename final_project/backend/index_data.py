67ce283aa7fa493e2fbc3345153746ee43978848
Imad Saddik <simad3647@gmail.com>
2024-11-18 08:52:19 +0100

    Reuse get_es_client in index_data

--- final_project/backend/index_data.py
+++ final_project/backend/index_data.py
@@ -1,34 +1,18 @@
-import time
 import json
 
 from tqdm import tqdm
 from typing import List
 from pprint import pprint
 from config import INDEX_NAME
+from utils import get_es_client
 from elasticsearch import Elasticsearch
 
 
 def index_data(documents: List[dict]):
-    es = _get_es_client(max_retries=5, sleep_time=5)
+    es = get_es_client(max_retries=5, sleep_time=5)
     _create_index(es=es, index_name=INDEX_NAME)
     _insert_documents(es=es, index_name=INDEX_NAME, documents=documents)
     pprint(f'Indexed {len(documents)} documents into Elasticsearch index "{INDEX_NAME}"')
-
-
-def _get_es_client(max_retries: int = 5, sleep_time: int = 5) -> Elasticsearch:
-    i = 0
-    while i < max_retries:
-        try:
-            es = Elasticsearch('http://localhost:9200')
-            client_info = es.info()
-            pprint('Connected to Elasticsearch!')
-            pprint(f'Cluster info:\n{client_info}')
-            return es
-        except Exception:
-            pprint('Could not connect to Elasticsearch, retrying...')
-            time.sleep(sleep_time)
-            i += 1
-    raise ConnectionError("Failed to connect to Elasticsearch after multiple attempts.")
 
 
 def _create_index(es: Elasticsearch):
