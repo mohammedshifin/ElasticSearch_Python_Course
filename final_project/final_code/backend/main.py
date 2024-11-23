5e5885606456b82ca855254d3dd9c04c24f26c73
Imad Saddik <simad3647@gmail.com>
2024-11-23 10:42:38 +0100

    Fixed a bug where a search query does not exist in the inversed index

--- final_project/final_code/backend/main.py
+++ final_project/final_code/backend/main.py
@@ -2,6 +2,7 @@ from config import INDEX_NAME
 from utils import get_es_client
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 
@@ -17,20 +18,28 @@ app.add_middleware(
 
 @app.get("/api/v1/search/")
 async def search(search_query: str, skip: int = 0, limit: int = 10) -> dict:
-    es = get_es_client(max_retries=5, sleep_time=5)
-    response = es.search(
-        index=INDEX_NAME,
-        body={
-            "query": {
-                "multi_match": {
-                    "query": search_query,
-                    "fields": ["title", "explanation"]
-                }
+    try:
+        es = get_es_client(max_retries=1, sleep_time=0)
+        
+        response = es.search(
+            index=INDEX_NAME,
+            body={
+                "query": {
+                    "multi_match": {
+                        "query": search_query,
+                        "fields": ["title", "explanation"],
+                    }
+                },
+                "from": skip,
+                "size": limit
             },
-            "from": skip,
-            "size": limit
-        },
-        filter_path=["hits.hits._source,hits.hits._score"]
-    )
-    hits = response["hits"]["hits"]
-    return {"hits": hits}
+            filter_path=["hits.hits._source,hits.hits._score"]
+        )
+        if not response:
+            return {"hits": []}
+        
+        hits = response["hits"]["hits"]
+        return {"hits": hits}
+    except ConnectionError:
+        error_message = "Could not connect to Elasticsearch, make sure that the cluster is up and running."
+        return HTMLResponse(content=error_message, status_code=500)
